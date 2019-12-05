a6e9690f7a7709e715b1bc8991c4b43a1ed0a457
Abhishek <abhishekasdzxc@gmail.com>
2019-12-05 21:27:33 +0530

    Update client.py

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ url_gen = lambda dims: f"http://192.168.1.102:8090/random?size={'x'.join([str(k)
 dims=[10,20,30]
 data = json.loads(requests.get(url_gen(dims)).text)
 arr = np.asarray(data['array'])
-print("shape : ",arr.shape) #For Verification
+print("shape : ", arr.shape) #For Verification
 """
 Output: shape : (10,20,30)
 """
